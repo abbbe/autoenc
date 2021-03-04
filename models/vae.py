0ee8a0adb65ae141820ee860476e459479dcc3dc
Alexandre Bezrouchko <alexandre.bezroutchko@gmail.com>
2021-03-05 00:49:19 +0100

    tried to implement more sensible architecture of AE

--- models/vae.py
+++ models/vae.py
@@ -64,20 +64,11 @@ def build_encoder(input_img, latentDim, build_params):
 
     #print("before enc conv=" + str(x))
     for conv in build_params['convs']:
-        x = layers.Conv2D(conv, (3, 3), activation='relu', padding='same')(x)
-        x = layers.MaxPooling2D((2, 2), padding='same')(x)
-
-#    x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-#    x = layers.MaxPooling2D((2, 2), padding='same')(x)
-#    x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-#    x = layers.MaxPooling2D((2, 2), padding='same')(x)
-#    x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-#    x = layers.MaxPooling2D((2, 2), padding='same')(x)
-    #print("after enc conv=" + str(x))
+        x = layers.Conv2D(conv, kernel_size=3, activation='relu', padding='same')(x)
 
     volumeSize = K.int_shape(x)
     x = layers.Flatten()(x)
-    #x = layers.Dense(latentDim)(x)
+    #x = layers.Dense(16, activation='relu')(x)
 
     z_mean = layers.Dense(latentDim, name="z_mean")(x)
     z_log_var = layers.Dense(latentDim, name="z_log_var")(x)
@@ -89,24 +80,16 @@ def build_encoder(input_img, latentDim, build_params):
 def build_decoder(encoded, volumeSize, build_params):
     x = encoded
 
-    x = layers.Dense(np.prod(volumeSize[1:]))(encoded)
+    x = layers.Dense(np.prod(volumeSize[1:]), activation='relu')(encoded)
     x = layers.Reshape((volumeSize[1], volumeSize[2], volumeSize[3]))(x)
 
     #print("before dec conv=" + str(x))
     rev_convs = list(build_params['convs'])
     rev_convs.reverse()
     for conv in rev_convs:
-        x = layers.Conv2D(conv, (3, 3), activation='relu', padding='same')(x)
-        x = layers.UpSampling2D((2, 2))(x)
-    #x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    #x = layers.UpSampling2D((2, 2))(x)
-    #x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    #x = layers.UpSampling2D((2, 2))(x)
-    #x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-    #x = layers.UpSampling2D((2, 2))(x)
-    
-    x = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-    #print("after dec conv=" + str(x))
+        x = layers.Conv2DTranspose(conv, kernel_size=3, activation='relu', padding='same')(x)
+
+    x = layers.Conv2DTranspose(1, kernel_size=1, activation='sigmoid', padding='same')(x)
 
     decoded = x
     decoder = keras.Model(encoded, decoded, name="decoder")
@@ -115,9 +98,11 @@ def build_decoder(encoded, volumeSize, build_params):
 def build_autoencoder(inputShape, latentShape, build_params):
     inputs = layers.Input(shape=inputShape)
     encoder_model, volumeSize = build_encoder(inputs, latentShape, build_params)
+    encoder_model.summary()
 
     latent = layers.Input(shape=latentShape)
     decoder_model = build_decoder(latent, volumeSize, build_params)
+    decoder_model.summary()
 
     autoencoder_model = VAE(encoder_model, decoder_model, name="autoencoder")
     autoencoder_model.compile(optimizer=keras.optimizers.Adam())
